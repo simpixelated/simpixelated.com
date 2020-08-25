5fed3e1e4fee1888d501b1297dd8c22f73d69e91
Jordan Kohl <kohl.jordan@gmail.com>
2020-08-25 10:20:42 -0700

    updates lighthouse github action and moves lighthouse config into .github folder

---
+++ .github/lighthouserc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  ci: {
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:recommended",
+      assertions: {
+        "color-contrast": "off",
+        "errors-in-console": "off",
+        "installable-manifest": "off",
+        "maskable-icon": "off",
+        "offline-start-url": "off",
+        "service-worker": "off",
+        "splash-screen": "off",
+        "themed-omnibox": "off",
+        "unused-javascript": "off",
+        "uses-responsive-images": "off",
+        "works-offline": "off",
+        "tap-targets": "off",
+      },
+    },
+  },
+}
