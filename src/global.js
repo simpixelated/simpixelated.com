25441f9acbe2a593014b99a302924c6bf449176f
Jordan Kohl <jordan.kohl@segment.com>
2023-01-02 11:10:12 -0800

    styles toggle color mode button

--- src/global.js
+++ src/global.js
@@ -8,12 +8,15 @@ let colorMode = storedColorMode ?? preferredColorMode
 
 document.documentElement.classList.add(colorMode)
 
-document.getElementById("toggle-dark").addEventListener("click", e => {
+const toggle = document.getElementById("toggle-color-mode")
+toggle.addEventListener("click", e => {
   colorMode = colorMode === "dark" ? "light" : "dark"
   if (colorMode === "dark") {
+    toggle.classList.add("light")
     document.documentElement.classList.remove("light")
     document.documentElement.classList.add("dark")
   } else {
+    toggle.classList.remove("light")
     document.documentElement.classList.add("light")
     document.documentElement.classList.remove("dark")
   }
