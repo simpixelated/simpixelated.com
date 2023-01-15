b8716d2e578e91d64ddbc456f50a65586be8b511
Jordan Kohl <jordan.kohl@segment.com>
2023-01-15 12:40:30 -0800

    creates social share layout and build mode

--- src/posts/posts.11tydata.js
+++ src/posts/posts.11tydata.js
@@ -1,9 +1,17 @@
-module.exports = {
-  eleventyComputed: {
-    socialImage: data => {
-      return `${data.site.url}/static/banners/${data.title
-        .replaceAll(" ", "-")
-        .toLowerCase()}-social-card.png`
+module.exports = () => {
+  const settings = {
+    layout: "post",
+    permalink: "/{{ title | slugify }}/",
+    tags: "posts",
+    eleventyComputed: {
+      socialImage: data =>
+        `${data.site.url}/static/banners/${data.permalink}-social-card.png`,
     },
-  },
+  }
+
+  if (process.env.NODE_ENV == "social") {
+    settings.layout = "post-social"
+  }
+
+  return settings
 }
