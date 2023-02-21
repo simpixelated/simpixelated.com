fb372e6060cb44b2015527c55cae7b7bcf44e786
Jordan Kohl <kohl.jordan@gmail.com>
2023-02-21 08:53:23 -0800

    updates to latest lighthoue plugin for github ci (#144)
    * updates to latest lighthoue plugin for github ci
    
    * upgrades wait to v1.4 for node16 support
    
    * removes checkout
    
    * checkout needed for lighthouse rc
    
    * adds budgets
    
    * testing assertions
    
    * switches to no-pwa preset
    
    * updates assertions
    
    * fix assertion

--- .github/lighthouserc.js
+++ .github/lighthouserc.js
@@ -4,27 +4,36 @@ module.exports = {
       target: "temporary-public-storage",
     },
     assert: {
-      preset: "lighthouse:recommended",
+      preset: "lighthouse:no-pwa",
       assertions: {
-        "color-contrast": "off",
-        "errors-in-console": "off",
-        "installable-manifest": "off",
-        "maskable-icon": "off",
-        "offline-start-url": "off",
-        "service-worker": "off",
-        "splash-screen": "off",
-        "themed-omnibox": "off",
-        "unused-javascript": "off",
-        "uses-responsive-images": "off",
-        "works-offline": "off",
-        "tap-targets": "off",
+        "uses-responsive-images": "warn",
         "is-crawlable": "off",
-        "unsized-images": "off",
-        canonical: "warn",
-        "offscreen-images": "warn", // incorrectly flags negative margin avatar in header
         "uses-rel-preconnect": "warn", // flags Netlify scripts on deploy preview
-        "non-composited-animations": "warn", // flags "Back to home" anchor
+        "csp-xss": "warn",
+        deprecations: "warn",
       },
+    },
+    settings: {
+      budgets: [
+        {
+          timings: [
+            {
+              metric: "interactive",
+              budget: 3000,
+            },
+            {
+              metric: "first-meaningful-paint",
+              budget: 2000,
+            },
+          ],
+          resourceSizes: [
+            {
+              resourceType: "total",
+              budget: 500,
+            },
+          ],
+        },
+      ],
     },
   },
 }
